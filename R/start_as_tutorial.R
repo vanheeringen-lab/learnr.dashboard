f4435b7c167bbd55c5d34f8fe204399d01ceca12
siebrenf <siebrenf@gmail.com>
2020-11-30 21:45:30 +0100

    run tutorials in the background

--- R/start_as_tutorial.R
+++ R/start_as_tutorial.R
@@ -5,6 +5,23 @@ start_tutorial <- function(name = "test", package = "learnr.proto") {
 }
 
 #' @export
+start_background_tutorial <- function(name = "test", package = "learnr.proto", libpath = "/home/siebrenf/miniconda3/envs/learnr/lib/R/library", port = 8686) {
+  #' start tutorial from package in library as a background process
+  #' may require a unique port
+  cmd_start <- "R -e \""
+  set_library <- paste0("learnr.dashboard::set_lib_paths(\'", libpath, "\')")
+  cmd_next <- "; "
+  set_tutorial <- paste0("learnr::run_tutorial(\'", name, "\', \'", package, "\', shiny_args = list(launch.browser = FALSE, port=", port, "))")
+  cmd_end <- "\""
+
+  cmd = paste0(cmd_start, set_library, cmd_next, set_tutorial, cmd_end)
+  system(cmd, wait = FALSE)
+
+  url = paste0("http://127.0.0.1:", port)
+  browseURL("http://127.0.0.1:8686")
+}
+
+#' @export
 start_dev_tutorial <- function(
   lesson = "test",
   dev_path = "/home/siebrenf/git/edu/learnr.proto",
