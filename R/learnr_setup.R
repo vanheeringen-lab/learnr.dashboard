c25ee675d2a555d43b17898111409322833f0f6e
siebrenf <siebrenf@gmail.com>
2021-01-06 16:44:37 +0100

    refactor start_background_tutorial()

--- R/learnr_setup.R
+++ R/learnr_setup.R
@@ -1,26 +1,48 @@
 #' @export
-learnr_setup <- function(renv_lib, system_libs=NULL){
-  #' switch library paths to the renv libraries.
-  #' renv installs new packages in a dedicated renv_lib.
-  #' renv symlinks the base system libraries in a dir in /tmp (with 755 permissions)
-  #' this folder is required, but can be found automatically if it exists.
-  #' If it does not exist, the system libraries are used directly.
+learnr_setup <- function(renv_lib=NULL, system_libs=NULL, load=TRUE){
+  #' Assert that a renv library is available.
+  #' If load=TRUE, set the renv library and system libraries as only libraries.
+  #' If load=FALSE, return the renv library and system libraries.
+  #' Uses the renv system library if available, else use the default system libraries.
 
+  # find renv library in library paths
+  if ( is.null(renv_lib) ){
+    renv_path_substring <- file.path("renv", "library")
+    renv_lib <- .libPaths()[grepl(renv_path_substring, .libPaths(), fixed=T)]
+  }
+  if ( length(renv_lib)<1 ){
+    stop("No renv library found! Add one with .libPaths('/path/to/renv/library')")
+  }
+  if ( length(renv_lib)>1 ){
+    stop("More than one renv library found! Please restart R with .rs.restartR()")
+  }
+  if ( !(file.access(renv_lib, 4) == 0 && file.access(renv_lib, 1) == 0) ){
+    stop("Incorrect permissions to renv library!
+         Please tell the admin to run 'chmod -R 755' on the renv library and upstream protected directories.")
+  }
+
+  # find the system libraries
   if ( is.null(system_libs) ){
-    # find the renv-system-library
+    # check for a renv-system-library
     for (tmpdir in list.files("/tmp")){
       if ("renv-system-library" %in% list.files(paste0("/tmp/", tmpdir))){break}
     }
     system_libs = paste0("/tmp/", tmpdir, "/renv-system-library")
 
-    # use raw system libraries if renv-system-library does not exist
-    if (!dir.exists(system_libs)){system_libs = .libPaths()[grepl("usr", .libPaths(), fixed=T)]}
+    # use the default system libraries if renv-system-library does not exist
+    if (!dir.exists(system_libs)){
+      system_libs = .libPaths()[grepl("usr", .libPaths(), fixed=T)]
+    }
   }
 
-  # set libraries
-  learnr.dashboard::set_lib_paths(c(renv_lib, system_libs))
+  if (load){
+    # set libraries
+    learnr.dashboard::set_lib_paths(c(renv_lib, system_libs))
 
-  # unload all packages (prevents mixing the libraries)
-  suppressPackageStartupMessages(invisible(lapply(names(sessionInfo()$loadedOnly), require, character.only = TRUE)))
-  suppressWarnings(invisible(lapply(paste0('package:', names(sessionInfo()$otherPkgs)), detach, character.only=TRUE, unload=TRUE, force=TRUE)))
+    # unload all packages (prevents mixing the libraries)
+    suppressPackageStartupMessages(invisible(lapply(names(sessionInfo()$loadedOnly), require, character.only = TRUE)))
+    suppressWarnings(invisible(lapply(paste0('package:', names(sessionInfo()$otherPkgs)), detach, character.only=TRUE, unload=TRUE, force=TRUE)))
+  } else {
+    return(c(renv_lib, system_libs))
+  }
 }
--- R/start_as_tutorial.R
+++ R/start_as_tutorial.R
@@ -5,39 +5,27 @@ start_tutorial <- function(name = "test", package = "learnr.proto") {
 }
 
 #' @export
-start_background_tutorial <- function(name = "test", package = "learnr.proto", libpaths = NULL, rpath = NULL, r_flags = NULL, logfile = NULL, port = NULL) {
+start_background_tutorial <- function(name = "test", package = "learnr.proto", rpath = NULL, r_args = NULL, logfile = NULL, port = NULL) {
   #' start tutorial from package in library as a background process.
-  #' the called process should contain an inbuilt terminate on session end.
+  #' the tutorial should contain an inbuilt terminate on session end/timeout.
 
-  if ( is.null(libpaths) ){
-    libpaths = .libPaths()
-  }
-  if ( !grepl("renv", libpaths[1], fixed=T) ){
-    stop("The libpaths must contain the renv project library.")
-  }
-  liblist = ""
-  for (lib in libpaths){
-    liblist = paste0(liblist, "'", lib, "', ")
-  }
-  liblist = substr(liblist, 1, nchar(liblist)-2)
-  liblist = paste0("c(", liblist, ")")
+  libpaths = learnr.dashboard::learnr_setup(load=F)  # check if a renv library is available
+  renv_lib = paste0("'", libpaths[1], "'")
 
   if ( is.null(rpath) ){
-    rpath <- "R"
+    rpath <- file.path(R.home(), "bin", "R")  # same as Rstudio server uses
   }
 
-  if ( is.null(r_flags) ){
-    r_flags = "--vanilla -q"
+  if ( is.null(r_args) ){
+    r_args = "--vanilla -q"  # cannot contain '-e'
   }
 
+  # stores sdterr (app loaded/crashed messages)
   if ( is.null(logfile) ){
-    logfile <- tempfile(
-      pattern = "learnr_log_",
-      tmpdir = tempdir(check=T)
-    )
+    logfile <- tempfile("learnr_log_", tempdir(check=T))
   }
 
-  shiny_args = "list(launch.browser = FALSE)"
+  shiny_args = "list(launch.browser=FALSE)"
   if ( !is.null(port) ){
     shiny_args = paste0(
       "list(launch.browser=FALSE, port=", port, ")"
@@ -46,25 +34,25 @@ start_background_tutorial <- function(name = "test", package = "learnr.proto", l
 
   # start the app via a background system call
   cmd = paste0(
-    rpath, " ", r_flags, " -e \"",
-      "learnr.dashboard::set_lib_paths(", liblist, ")",
+    rpath, " ", r_args, " -e \"",
+      ".libPaths(", renv_lib,")",
       "; ",
-      "learnr::run_tutorial('", name, "', '", package, "', shiny_args = ", shiny_args, ")",
+      "learnr.dashboard::learnr_setup()",
+      "; ",
+      "learnr::run_tutorial('", name, "', '", package, "', shiny_args=", shiny_args, ")",
+      "; ",
+      "q('no')",  # stop this R process after the tutorial has stopped
     "\" 2> ", logfile , " 1> /dev/null"
   )
-  # # the bash command as it would run for me:
-  # cmd = paste0(
-  #   "R --vanilla -q -e \"",
-  #     "learnr.dashboard::set_lib_paths(c('/home/siebrenf/git/edu/learnr.proto/renv/library/R-3.6/x86_64-pc-linux-gnu', '/tmp/Rtmp87oD66/renv-system-library'));",
-  #     "learnr::run_tutorial('test', 'learnr.proto', shiny_args = list(launch.browser = FALSE))",
-  #   "\" 2> ~/learnr.log 1> /dev/null"
-  # )
   system(cmd, wait=FALSE)
+  learnr.dashboard:::.open_browser(name, logfile)
+}
 
-  # start the browser when the app has loaded
-  # the log will contain "Listening on ..." (and the port used).
-  log_lines_parsed = 0
+.open_browser <- function(name, logfile, address="http://127.0.0.1") {
+  #' start a browser when the app has loaded
+
   cat("Loading ", name, "..")
+  log_lines_parsed = 0
   still_loading = TRUE
   app_online = FALSE
   while(TRUE){
@@ -74,21 +62,17 @@ start_background_tutorial <- function(name = "test", package = "learnr.proto", l
     log = log[sapply(log, nchar) > 0]  # remove empty lines
     len = length(log)
 
-    # add newline before starting to print the log
     if ( still_loading && len == 0 ) {
-      cat(".")
+      cat(".")  # indicate that app is still loading
     } else if ( still_loading && len > 0 ){
-      cat('\n\n')
+      cat('\n\n')  # add newline before starting to print the log
       still_loading = FALSE
     } else if ( len > log_lines_parsed ){
       for (line in log[log_lines_parsed+1: len]){
         cat(line, "\n")
 
         if ( startsWith(line, "Listening on ") ){
-          # get port if not specified
-          if ( is.null(port) ){
-            port = strsplit(line, ":", fixed=TRUE)[[1]][3]
-          }
+          port = strsplit(line, ":", fixed=TRUE)[[1]][3]
           app_online = TRUE
           break  # break from for loop
         }
@@ -100,90 +84,11 @@ start_background_tutorial <- function(name = "test", package = "learnr.proto", l
       log_lines_parsed <- len
 
       if ( app_online ){
-        url = paste0("http://127.0.0.1:", port)
+        url = paste0(address, ":", port)
         browseURL(url)
         break  # break from while loop
       }
     }
   }
-
   unlink(logfile)
 }
-
-# #' @export
-# start_background_tutorial <- function(name = "test", package = "learnr.proto", libpath = NULL, rpath = NULL, port = 8686) {
-#   #' start tutorial from package in library as a background process.
-#   #' the called process should contain an inbuilt terminate on session end.
-#
-#   if ( is.null(libpath) ){
-#     libpath = .libPaths()[1]  # libpath = "/home/siebrenf/miniconda3/envs/learnr/lib/R/library"
-#   }
-#   if ( is.null(rpath) ){
-#     # dirs <- strsplit(libpath, "/")[[1]]
-#     # l <- length(dirs)-3
-#     # new_dirs <- do.call(c, list(dirs[1:l], list("bin", "R")))
-#     # rpath <- paste(new_dirs, collapse = '/')
-#     rpath <- "R"  # system R
-#   }
-#
-#   # start the app via a background system call
-#   cmd_start <- paste0(rpath, " -e \"")
-#   set_library <- paste0("learnr.dashboard::set_lib_paths(\'", libpath, "\')")
-#   cmd_next <- "; "
-#   set_tutorial <- paste0("learnr::run_tutorial(\'", name, "\', \'", package, "\', shiny_args = list(launch.browser = FALSE, port=", port, "))")
-#   cmd_end <- "\""
-#
-#   cmd = paste0(cmd_start, set_library, cmd_next, set_tutorial, cmd_end)
-#   system(cmd, wait = FALSE)
-#
-#   # give the server some time to boot up
-#   Sys.sleep(2.5)
-#
-#   # open the browser window to the app
-#   url = paste0("http://127.0.0.1:", port)
-#   browseURL(url)
-# }
-
-# #' @export
-# start_dev_tutorial <- function(
-#   lesson = "test",
-#   dev_path = "/home/siebrenf/git/edu/learnr.proto",
-#   install_lib_path = "/home/siebrenf/miniconda3/envs/learnr/lib/R/library"
-#   ) {
-#   #' dev only
-#   #' reinstall pkg in dev_path to install_lib_path
-#   #' then run lesson as tutorial
-#
-#   learnr.dashboard::install_to_lib(
-#     pkg = dev_path,
-#     quick = T,
-#     libpath = install_lib_path
-#   )
-#
-#   # run as tutorial
-#   learnr::run_tutorial(lesson, package = basename(dev_path))
-#
-# }
-
-# #' @export
-# update_dev_tutorial <- function(
-#   dev_path = "/home/siebrenf/git/edu/learnr.proto",
-#   libpath = NULL
-#   ) {
-#
-#   original_libpaths = .libPaths()
-#   if (is.null(libpath)) {libpath = .libPaths()[1]}
-#
-#   learnr.dashboard::set_lib_paths(libpath)
-#   # update the tutorial
-#   devtools::install(
-#     pkg=dev_path,
-#     dependencies=F,
-#     build_vignettes=F,
-#     quiet=T,
-#     force=F
-#   )
-#
-#   learnr.dashboard::set_lib_paths(original_libpaths)
-#
-# }
